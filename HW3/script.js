b5d2f1ae865124e515dd059d310c38553b6b3c33
010276JCT <160989813+010276JCT@users.noreply.github.com>
2024-03-29 10:22:35 +0800

    Update script.js

--- HW3/script.js
+++ HW3/script.js
@@ -33,14 +33,15 @@ for(let i=0;i<N;i++)
 
     // TODO: 如果發生碰撞(畫布寬canvas.width, 畫布高canvas.height)，則改變速度(dx, dy)和顏色(color)
 
-  for(let i=0;i<N;i++)
-      { if(x[i]<0|| x[i]>canvas.width) dx[i]=-dx[i];
-        if(y[i]<0|| y[i]>canvas.height) dy[i]=-dy[i];
-      }   
-
+  for(let i = 0; i < N; i++)
+  {
+      if(x[i]<0|| x[i]>canvas.width) dx[i]=-dx[i];
+      if(y[i]<0|| y[i]>canvas.height) dy[i]=-dy[i];
+  }
+  //分開列式才不會讓i和j互相抵消
   for(let i=0;i<N;i++)
     for(let j=0;j<N;j++)
-    {
+      {
         if((x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j])<(r[i]+r[j])*(r[i]+r[j]))
           [dx[i],dx[j],dy[i],dy[j]]=[dx[j],dx[i],dy[j],dy[i]];
       }
